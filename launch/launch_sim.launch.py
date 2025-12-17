f890ccdf6963c7e0953faae3fefff307e0c81685
carlobeni <carlosbenitez.dev.acct@gmail.com>
2025-12-17 12:33:08 -0300

    add new launchers

---
+++ launch/launch_sim.launch.py
@@ -0,0 +1,178 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+import xacro
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+from launch_ros.actions import Node
+
+
+
+def generate_launch_description():
+
+
+    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
+    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
+
+    package_name='phantom_uttubot' #<--- CHANGE ME
+
+    rsp = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
+    )
+
+    joystick = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','joystick.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+
+
+    twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
+    twist_mux = Node(
+            package="twist_mux",
+            executable="twist_mux",
+            parameters=[twist_mux_params, {'use_sim_time': True}],
+            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
+        )
+
+
+    default_world = os.path.join(
+        get_package_share_directory(package_name),
+        'worlds',
+        'obstacles3.world'
+        )    
+    
+    world = LaunchConfiguration('world')
+
+    world_arg = DeclareLaunchArgument(
+        'world',
+        default_value=default_world,
+        description='World to load'
+        )
+
+    # Include the Gazebo launch file, provided by the ros_gz_sim package
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
+                    launch_arguments={'gz_args': ['-r -v4 ', world], 'on_exit_shutdown': 'true'}.items()
+             )
+
+    # Run the spawner node from the ros_gz_sim package. The entity name doesn't really matter if you only have a single robot.
+    spawn_entity = Node(package='ros_gz_sim', executable='create',
+                        arguments=['-topic', 'robot_description',
+                                   '-name', 'phantom_uttubot',
+                                   '-z', '0.02'],
+                        output='screen')
+
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster"],
+    )
+
+    joint_trajectory_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_trajectory_controller"],
+    )
+
+
+
+
+    bridge_params = os.path.join(get_package_share_directory(package_name),'config','gz_bridge.yaml')
+    ros_gz_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            '--ros-args',
+            '-p',
+            f'config_file:={bridge_params}',
+        ]
+    )
+
+    ros_gz_image_bridge = Node(
+        package="ros_gz_image",
+        executable="image_bridge",
+        arguments=["/camera/image_raw"]
+    )
+
+
+    heading_estimator = Node(
+    package='phantom_uttubot',
+    executable='noise_compute_heading.py',
+    name='heading_estimator',
+    output='screen',
+    )
+    # OBS: Calcula el heading
+
+    gps_to_local_xy = Node(
+    package='phantom_uttubot',
+    executable='noise_gps_to_local_xy.py',
+    name='gps_to_local_xy',
+    output='screen',
+    )
+    # OBS: Coordenadas metricas respecto al LAR donde la X+ es el polo magnetico con +Y
+
+
+    corrected_stimate_xy = Node(
+    package='phantom_uttubot',
+    executable='corrected_stimate_xy.py',
+    name='corrected_stimate_xy',
+    output='screen',
+    )
+    # OBS: Partiendo de una posicion inicial, calcula la posicion actual en (x,y)
+
+    # cinematica inversa
+    hexapod_low_level_control = Node(
+    package='phantom_uttubot',
+    executable='hexapod_low_level_control.py',
+    name='hexapod_low_level_control',
+    output='screen'
+    )
+    # OBS: El robot se mueve publicando en hl/cmd tipic de control
+
+    ultrasonic_range = Node(
+    package='phantom_uttubot',
+    executable='ultrasonic_range.py',
+    name='ultrasonic_range',
+    output='screen'
+    )
+    # OBS: Transorma del RANGE a longitudes del ultrasonico en m, si no detecta nada manda -1
+
+    ir_emulator = Node(
+    package='phantom_uttubot',
+    executable='ir_gazebo_ros_emulator.py',
+    name='ir_emulator',
+    output='screen'
+    )
+    # OBS: Emula los sensores IR y publica sus estados
+
+
+
+    # Launch them all!
+    return LaunchDescription([
+        rsp,
+        joystick,
+        twist_mux,
+        world_arg,
+        gazebo,
+        spawn_entity,
+        joint_state_broadcaster_spawner,
+        joint_trajectory_controller_spawner,
+        ros_gz_bridge,
+        ros_gz_image_bridge,
+        heading_estimator,
+        gps_to_local_xy,
+        corrected_stimate_xy,
+        ultrasonic_range,
+        ir_emulator,
+        hexapod_low_level_control
+    ])
---
+++ launch/social_robot.launch.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+import os
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+
+    pkg_name = "phantom_uttubot"
+
+    # ==========================================================
+    # ARGUMENTOS
+    # ==========================================================
+    image_width  = LaunchConfiguration("image_width")
+    image_height = LaunchConfiguration("image_height")
+    camera_frame = LaunchConfiguration("camera_frame")
+
+    declare_image_width = DeclareLaunchArgument(
+        "image_width",
+        default_value="640",
+        description="Camera image width"
+    )
+
+    declare_image_height = DeclareLaunchArgument(
+        "image_height",
+        default_value="480",
+        description="Camera image height"
+    )
+
+    declare_camera_frame = DeclareLaunchArgument(
+        "camera_frame",
+        default_value="camera_optical_link",
+        description="Camera optical frame"
+    )
+
+    # ==========================================================
+    # CÁMARA (v4l2)
+    # ==========================================================
+    camera_node = Node(
+        package="v4l2_camera",
+        executable="v4l2_camera_node",
+        name="camera",
+        output="screen",
+        parameters=[{
+            "image_size": [640, 480],
+            "camera_frame_id": "camera_optical_link",
+            "device": "/dev/video0"
+        }]
+    )
+    # ==========================================================
+    # NODO DE GESTOS
+    # ==========================================================
+    hand_gesture_node = Node(
+        package=pkg_name,
+        executable="hand_gesture_node.py",
+        name="hand_gesture_node",
+        output="screen",
+        remappings=[
+            ("/image_raw", "/image_raw")  # explícito, por claridad
+        ]
+    )
+
+    # ==========================================================
+    # NODO DE POSES SOCIALES
+    # ==========================================================
+    social_motion_node = Node(
+        package=pkg_name,
+        executable="social_pose_node.py",
+        name="social_motion_node",
+        output="screen"
+    )
+
+    # ==========================================================
+    # LAUNCH
+    # ==========================================================
+    return LaunchDescription([
+        declare_image_width,
+        declare_image_height,
+        declare_camera_frame,
+
+        camera_node,
+        hand_gesture_node,
+        social_motion_node,
+    ])
